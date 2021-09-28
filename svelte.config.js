dd24d64593bad7fbeefbd971070f9fe418df13c0
Rodney Lab <codesign@rodneylab.com>
2021-09-28 04:59:04 +0100

    fix: 💫 add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,6 +9,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
+    adapter: adapter(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte'
   }
